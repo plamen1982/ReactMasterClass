0e75ed1ccf73f0960e6909a41338983044f44139
plamen1982 <plamen.hristov@ymail.com>
2019-10-02 18:59:58 +0300

    Simple composition with Button

--- Training/ReactAdvanced/Component/component/src/App.js
+++ Training/ReactAdvanced/Component/component/src/App.js
@@ -5,22 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BlackButton />
     </div>
   );
 }
 
+const Button = ({color}) => {
+  return(
+    <button>This is a {color} Button</button>
+  );
+} 
+
+const BlackButton = () => {
+  const color = 'Brown'
+  return (
+    <Button color={color}/>
+  );
+
+}
 export default App;
