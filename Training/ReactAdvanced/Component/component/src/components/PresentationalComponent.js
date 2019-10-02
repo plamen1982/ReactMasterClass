0a4d9b8745c53d21aa16a0c79ade58a1976c707c
plamen1982 <plamen.hristov@ymail.com>
2019-10-02 23:55:02 +0300

    delete comments in PresentationalComponent

--- Training/ReactAdvanced/Component/component/src/components/PresentationalComponent.js
+++ Training/ReactAdvanced/Component/component/src/components/PresentationalComponent.js
@@ -1,11 +1,5 @@
 import React from 'react';
-// const starWarsCharacters = [
-//     {id:1, name: 'Anakin Skywalker'},
-//     {id:2, name: 'Obi-Wan Kenovi'},
-//     {id:3, name: 'Luke Skywalker'},
-//     {id:4, name: 'Yoda'},
-//     {id:5, name: 'Han Solo'},
-// ]
+
 const ListView = ({ starWarsCharacters }) => (
     <div>
         {
