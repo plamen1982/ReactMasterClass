4983947eb9f8e253b828ff89d24e310e24fe509a
plamen1982 <plamen.hristov@ymail.com>
2019-10-06 10:56:56 +0300

    test lint-staged

--- Training/ReactAdvanced/create-react-app-practice/src/App.js
+++ Training/ReactAdvanced/create-react-app-practice/src/App.js
@@ -4,7 +4,6 @@ import styles from './App.module.css';
 function App() {
   return (
     <>
-    
       <div className={styles.error}>{process.env.REACT_APP_URL}</div>
       <div>Some content</div>
     </>
