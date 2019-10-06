0c17f6ed6faabce930ade7d341327ea6cda13d0a
plamen1982 <plamen.hristov@ymail.com>
2019-10-06 11:26:18 +0300

    test commit

--- Training/ReactAdvanced/create-react-app-practice/src/App.js
+++ Training/ReactAdvanced/create-react-app-practice/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import styles from './App.module.css';
 
 function App() {
+  const Component = () => {
+    return <div>Content</div>
+  }
   return (
     <>
       <div className={styles.error}>{process.env.REACT_APP_URL}</div>
       <div>Some content</div>
+      <Component />
     </>
   );
 }
