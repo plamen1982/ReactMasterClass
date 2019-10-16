3b3c8b65cb8734b425a64d34da1e9860209b97ac
plamen1982 <plamen.hristov@ymail.com>
2019-10-16 23:56:47 +0300

    add axios

--- Training/ReactAdvanced/6.react-redux-and-react-persist/src/actions/index.js
+++ Training/ReactAdvanced/6.react-redux-and-react-persist/src/actions/index.js
@@ -1,4 +1,5 @@
-import { upCounter, downCounter } from "./counter";
-import { login } from "./user";
+import { upCounter, downCounter } from './counter';
+import { login } from './user';
+import { todos } from './todos';
 
-export { upCounter, downCounter, login };
+export { upCounter, downCounter, login, todos };
