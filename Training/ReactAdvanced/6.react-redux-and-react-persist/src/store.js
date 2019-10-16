ecf50bc6cfbcd7a28e99e6000953cdca03651467
plamen1982 <plamen.hristov@ymail.com>
2019-10-16 23:37:28 +0300

    added thunk

--- Training/ReactAdvanced/6.react-redux-and-react-persist/src/store.js
+++ Training/ReactAdvanced/6.react-redux-and-react-persist/src/store.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
     key: 'root',
@@ -11,6 +12,11 @@ const persistConfig = {
     // whitelist: ['user']
 };
 
+const allStoreEnhancers = compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension && window.devToolsExtension()
+);
+
 const pReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(pReducer);
+export const store = createStore(pReducer, {}, allStoreEnhancers);
 export const persistor = persistStore(store);
