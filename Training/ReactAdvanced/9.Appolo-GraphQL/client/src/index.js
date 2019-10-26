9df10ed0419b911b90790f584ad55bcc79b8e2f6
plamen1982 <plamen.hristov@ymail.com>
2019-10-26 13:32:55 +0300

    02-Connect Data-Layer to View-Layer: Introducing React Apollo

--- Training/ReactAdvanced/9.Appolo-GraphQL/client/src/index.js
+++ Training/ReactAdvanced/9.Appolo-GraphQL/client/src/index.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import * as serviceWorker from './serviceWorker';
 import './style.css';
 
 const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 const httpLink = new HttpLink({
   uri: GITHUB_BASE_URL,
   headers: {
-    authorization: `Bearer ${
-      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-    }`,
-  },
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+  }
 });
-const test;
+
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache,
+  cache
 });
 
 ReactDOM.render(
-  <App />,
+  <ApolloClient>
+    <App />
+  </ApolloClient>,
   document.getElementById('root')
 );
-
-registerServiceWorker();
